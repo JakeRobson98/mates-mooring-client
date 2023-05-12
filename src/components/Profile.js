cb12acb8dc34d839aafb4076fd32d8ed29a2fb1e
jakerobson98 <jjoerobson@gmail.com>
2023-05-13 00:24:57 +1000

    cleaning home

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 
 const Profile = () => {
 
+  const { user } = useSelector((state) => state.user);
+
   return (
     <div className="container">
       <header className="jumbotron">
